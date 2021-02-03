eef2d714cbc727e211662d6f1bd750025ee4eb37
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-03 05:01:48 +0000

    Draw a square

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
 from turtle import Turtle, Screen
 
 turtle_sample = Turtle()
+turtle_sample.shape('turtle')
 
+# Draw a square
+for i in range(4):
+    turtle_sample.forward(100)
+    turtle_sample.right(90)
 
 # show the screen and close on click
 screen = Screen()
