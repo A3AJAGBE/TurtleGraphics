398012308d497df2992aa2b62825ef4797b3ca11
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-03 05:13:29 +0000

    Draw a dashed line

--- main.py
+++ main.py
@@ -1,13 +1,20 @@
-from turtle import Turtle, Screen
+import turtle
 
-turtle_sample = Turtle()
+turtle_sample = turtle.Turtle()
 turtle_sample.shape('turtle')
 
-# Draw a square
+"""# Draw a square
 for i in range(4):
     turtle_sample.forward(100)
-    turtle_sample.right(90)
+    turtle_sample.right(90)"""
+
+# Draw a dashed line
+for i in range(10):
+    turtle_sample.forward(10)
+    turtle_sample.penup()
+    turtle_sample.forward(10)
+    turtle_sample.pendown()
 
 # show the screen and close on click
-screen = Screen()
+screen = turtle.Screen()
 screen.exitonclick()
